6de416c11894da651864c2bcbbb30ff1c7388298
yabca1 <yabetsgobeze95@gmail.com>
2025-06-16 13:09:24 +0300

    commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Fullstack Developer", "Cybersecurity Researcher", "Networking Hobbyist"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
